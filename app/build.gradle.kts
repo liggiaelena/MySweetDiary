e358faed2dc4ff64f31b87bdb34fdb320594f3db
liggia cruz <liggia.cruz@olxbr.com>
2025-06-01 15:38:09 -0300

    feat: chips

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.android.material:material:1.11.0")
 }
